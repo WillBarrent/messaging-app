ab8c3537310f5d70839a7be182253f809fa63f50
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2026-06-13 15:57:15 +0500

    Add basic error handler for PUT and DELETE requests in message route
    If message doesn't exist, make sure you handle error properly and send adequate message

--- backend/controllers/message.ts
+++ backend/controllers/message.ts
@@ -1,6 +1,7 @@
-import { type Response, type Request } from "express";
+import { type Response, type Request, type NextFunction } from "express";
 import type { Message, NewMessage } from "../types";
 import messageModel from "../models/message.ts";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/client";
 
 const messagesGet = async (
   req: Request<{ receiverId: string; senderId: string }, unknown, string>,
@@ -28,29 +29,61 @@ const messagePost = async (
 const messagePut = async (
   req: Request<{ id: string }, unknown, NewMessage>,
   res: Response<Message>,
+  next: NextFunction,
 ) => {
-  const { id } = req.params;
-  const { content } = req.body;
+  try {
+    const { id } = req.params;
+    const { content } = req.body;
 
-  const updatedMessage = await messageModel.updateMessage({
-    id: Number(id),
-    content,
-  });
+    const updatedMessage = await messageModel.updateMessage({
+      id: Number(id),
+      content,
+    });
 
-  res.json(updatedMessage);
+    res.json(updatedMessage);
+  } catch (error: unknown) {
+    if (
+      error instanceof Error &&
+      error instanceof PrismaClientKnownRequestError
+    ) {
+      res.status(404).end(
+        JSON.stringify({
+          error: "Failed to update message: Message doesn't exist",
+        }),
+      );
+    } else {
+      next(error);
+    }
+  }
 };
 
 const messageDelete = async (
   req: Request<{ id: string }, unknown, unknown>,
   res: Response,
+  next: NextFunction,
 ) => {
-  const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-  await messageModel.deleteMessage({ id: Number(id) });
+    await messageModel.deleteMessage({ id: Number(id) });
 
-  res.json({
-    message: "Message has been removed",
-  });
+    res.json({
+      message: "Message has been removed",
+    });
+  } catch (error: unknown) {
+    if (
+      error instanceof Error &&
+      error instanceof PrismaClientKnownRequestError
+    ) {
+      res.status(404).end(
+        JSON.stringify({
+          error: "Failed to delete message: Message doesn't exist",
+        }),
+      );
+    } else {
+      next(error);
+    }
+  }
 };
 
 export default {
