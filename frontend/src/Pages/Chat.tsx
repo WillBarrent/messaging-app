033739574179612fd09caf377e040dc80e6fc00d
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2026-06-18 20:53:20 +0500

    Make styles for chat panel where all messages are displayed

--- frontend/src/Pages/Chat.tsx
+++ frontend/src/Pages/Chat.tsx
@@ -2,13 +2,87 @@ import React, { useContext, useState } from "react";
 import UserContext from "../UserContext";
 import type { Chat, UserContextType } from "../types";
 import styled from "styled-components";
+import { RiSendPlaneFill } from "react-icons/ri";
+import { format } from "date-fns";
 
-const Chat = styled.div`
+const Wrapper = styled.div`
   flex: 1;
   border: 5px solid black;
   margin: 20px;
-  padding: 20px 40px;
+
+  display: flex;
+  flex-direction: column;
+`;
+
+const Messages = styled.div`
+  flex: 1;
   overflow-y: auto;
+
+  display: flex;
+  flex-direction: column-reverse;
+  gap: 10px;
+  justify-content: flex-start;
+
+  padding: 20px;
+`;
+
+const MessageForm = styled.form`
+  width: 100%;
+  padding: 10px 20px;
+  margin-bottom: 0;
+
+  display: flex;
+`;
+
+const MessageInput = styled.input`
+  flex: 1;
+  outline: none;
+  border: 3px solid black;
+  border-right: 0px;
+  padding: 5px;
+  font-family: inherit;
+  font-size: 15px;
+`;
+
+const MessageSubmitButton = styled.button`
+  display: flex;
+  padding: 5px;
+  outline: none;
+  border: none;
+  border-radius: 50%;
+  background-color: #000;
+
+  &:hover {
+    cursor: pointer;
+    background-color: rgba(0, 0, 0, 0.8);
+  }
+`;
+
+const MessageSentAt = styled.div`
+  font-size: 10px;
+  align-self: flex-end;
+`;
+
+const MessageSubmitButtonWrapper = styled.div`
+  padding: 5px;
+  border: 3px solid black;
+  border-left: 0px;
+`;
+
+const MessageDisplay = styled.div<{ $isSender?: boolean }>`
+  border: 3px solid black;
+  padding: 10px;
+
+  width: max-content;
+  max-width: 500px;
+
+  background-color: ${(props) => (props.$isSender ? "#000" : "#fff")};
+  color: ${(props) => (props.$isSender ? "#fff" : "#000")};
+  align-self: ${(props) => (props.$isSender ? "flex-end" : "flex-start")};
+
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
 `;
 
 const ChatDisplay = ({
@@ -25,7 +99,6 @@ const ChatDisplay = ({
     return <div>Loading...</div>;
   }
 
-  const chatter = chat.users[0].username;
   const chatterId = chat.users[0].id;
 
   const onSubmit = async (e: React.SyntheticEvent) => {
@@ -52,45 +125,37 @@ const ChatDisplay = ({
   };
 
   return (
-    <Chat>
-      <h1>Chat with {chatter}</h1>
-      <form onSubmit={onSubmit} style={{ marginBottom: "10px" }}>
-        <input
+    <Wrapper>
+      <Messages>
+        {chat.messages.map((message) => {
+          const isSender = message.senderId === user?.userId;
+          const sentAt = format(new Date(message.createdAt), "PP, p");
+
+          return (
+            <MessageDisplay $isSender={isSender} key={message.id}>
+              <div>{message.content}</div>
+              <MessageSentAt>{sentAt}</MessageSentAt>
+            </MessageDisplay>
+          );
+        })}
+      </Messages>
+
+      <MessageForm onSubmit={onSubmit}>
+        <MessageInput
           type="text"
-          placeholder="message"
+          placeholder="Type a message"
           value={message}
           onChange={(e) => {
             setMessage(e.target.value);
           }}
         />
-        <button type="submit">Send</button>
-      </form>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          gap: "10px",
-        }}
-      >
-        {chat.messages.map((message) => {
-          return (
-            <div
-              key={message.id}
-              style={{
-                border: "1px solid black",
-                padding: "10px",
-              }}
-            >
-              <div style={{ fontWeight: "bold" }}>
-                Sender:{" "}
-                {message.senderId === user?.userId ? user.username : chatter}
-              </div>
-              <div>Message: {message.content}</div>
-            </div>
-          );
-        })}
-      </div>
-    </Chat>
+        <MessageSubmitButtonWrapper>
+          <MessageSubmitButton type="submit">
+            <RiSendPlaneFill size={20} color="#fff" />
+          </MessageSubmitButton>
+        </MessageSubmitButtonWrapper>
+      </MessageForm>
+    </Wrapper>
   );
 };
 
