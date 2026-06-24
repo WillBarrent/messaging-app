a28a6750c5e8fee2f410fa271ca5b9d9f859e76d
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2026-06-24 20:19:19 +0500

    Create dedicated database model for working with chat

---
+++ backend/models/chat.ts
@@ -0,0 +1,30 @@
+import prisma from "../db/prisma";
+
+const getAllUsersChats = async ({ userId }: { userId: number }) => {
+  const chats = await prisma.chat.findMany({
+    where: {
+      users: {
+        some: {
+          id: userId,
+        },
+      },
+    },
+    include: {
+      users: {
+        where: {
+          NOT: {
+            id: userId,
+          },
+        },
+        select: {
+          id: true,
+          username: true,
+        },
+      },
+    },
+  });
+
+  return chats;
+};
+
+export default { getAllUsersChats };
--- backend/models/user.ts
+++ backend/models/user.ts
@@ -7,36 +7,4 @@ const getAllUsers = async (): Promise<PlainUser[]> => {
   return users.map(({ id, username }) => ({ id, username }));
 };
 
-const getAllUsersChats = async ({ userId }: { userId: number }) => {
-  const chats = await prisma.chat.findMany({
-    where: {
-      users: {
-        some: {
-          id: userId,
-        },
-      },
-    },
-    include: {
-      messages: {
-        orderBy: {
-          createdAt: "desc",
-        },
-      },
-      users: {
-        where: {
-          NOT: {
-            id: userId,
-          },
-        },
-        select: {
-          id: true,
-          username: true,
-        },
-      },
-    },
-  });
-
-  return chats;
-};
-
-export default { getAllUsers, getAllUsersChats };
+export default { getAllUsers };
